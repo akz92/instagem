e9ade3ddbec0b7af5247028ef6ca3dfcab210ebf
akz92 <aride.moulin@gmail.com>
2018-05-18 14:13:52 +0100

    Fix unauthenticated root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'home/show'
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  root 'home#show'
+  authenticated :user do
+    root 'home#show', as: :authenticated_root
+  end
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
 end
